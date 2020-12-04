d9ca0caf4b7586f26cd2de175321d9fa769b5826
Garik <garikfreedomyan@gmail.com>
2020-12-05 01:18:17 +0300

    added navbar logic

--- index.js
+++ index.js
@@ -14,11 +14,24 @@ app.set('views', 'views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Courses store',
+    isHome: true,
+  });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Courses',
+    isCourses: true,
+  });
+});
+
+app.get('/courses/add', (req, res) => {
+  res.render('add-course', {
+    title: 'Add course',
+    isAdd: true,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
